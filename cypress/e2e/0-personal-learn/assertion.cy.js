89bff7d5c966b595d81e1105e4ad66bb709242bf
prayogoferry <prayogoferry@gmail.com>
2023-07-16 19:51:32 +0700

    nu upload

---
+++ cypress/e2e/0-personal-learn/assertion.cy.js
@@ -0,0 +1,8 @@
+///<reference types = 'Cypress'/>
+
+describe ('Browser Actions', ()=>{
+    it('connect correct url', ()=>{
+        cy.visit('http:facebook.com')
+    })
+})
+
--- cypress/e2e/0-personal-learn/first.test.cy.js
+++ cypress/e2e/0-personal-learn/first.test.cy.js
@@ -2,13 +2,19 @@
 
 
 describe ("my first test", ()=> {
-    it('visit the kitchen sink', ()=>{
-        cy.visit('https://example.cypress.io')
-        cy.get('h1').contains("Kitchen Sink")
-        cy.contains('get').click()
-        cy.url().should('include', 'commands/querying')
+    // it('visit the kitchen sink', ()=>{
+    //     cy.visit('https://example.cypress.io')
+    //     cy.get('h1').contains("Kitchen Sink")
+    //     cy.contains('get').click()
+    //     cy.url().should('include', 'commands/querying')
 
-    })
+    // })
+
+    it('Connect correct Url', () => {
+        cy.visit('https://facebook.com')
+        cy.url().should('include', 'face')
+
+    });
     
     
 })
