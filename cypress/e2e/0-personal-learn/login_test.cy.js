ba3be23cd29996c0bd0096a18f62db9ad92b3b5b
prayogoferry <prayogoferry@gmail.com>
2023-07-18 00:45:15 +0700

    nu upload

--- cypress/e2e/0-personal-learn/login_test.cy.js
+++ cypress/e2e/0-personal-learn/login_test.cy.js
@@ -7,7 +7,12 @@ describe('working with inputs ',()=>{
         cy.url().should('include', 'login')
     })
 
-    it('', () => {
+    it('Shouls fill login field', () => {
+        cy.get('#user_login').clear().type('username')
+        cy.get('#user_password').clear().type('password')
+        cy.get('#user_remember_me').check()
+        cy.get('.btn').click(2)
+        
         
     });
 
