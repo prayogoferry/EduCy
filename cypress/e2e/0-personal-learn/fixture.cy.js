6b285cc5568debac89c92b3d233618d82b305065
prayogoferry <prayogoferry@gmail.com>
2023-07-20 20:42:21 +0700

    nu

--- cypress/e2e/0-personal-learn/fixture.cy.js
+++ cypress/e2e/0-personal-learn/fixture.cy.js
@@ -3,7 +3,7 @@
 describe ('Login ',()=>{
 
     beforeEach(()=>{
-      cy.visit('https://practicetestautomation.com/practice-test-login/')
+      cy.visit('https://practicetestautomation.com/practice-test-login/', {timeout:10000})
     //   cy.url().should('contain', 'facebook.com')
     })
 
@@ -20,16 +20,34 @@ describe ('Login ',()=>{
             cy.get('strong').should('contain.text', 'Congratulations' || 'successfully logged in')
             cy.get('.wp-block-button__link').should('be.visible')
         })
+        
+    });
 
     it("Negative Username test with fixture ", () => {
-        cy.get('#username').clear().type(username[2])
-        cy.get('#password').clear().type(password[1])
-        cy.get('#submit').click()
-    });
-        
+        cy.fixture("user").then(u=>{
+            let username = u.username
+            let password = u.password
 
+            cy.get('#username').clear().type(username[2])
+            cy.get('#password').clear().type(password[1])
+            cy.get('#submit').click()
+
+            cy.get('#error').should('contain.text', 'Your username is invalid!')
+        })  
         
-        
+    });
+
+    it("Negative Password test with fixture ", () => {
+        cy.fixture("user").then(u=>{
+            let username = u.username
+            let password = u.password
+
+            cy.get('#username').clear().type(username[1])
+            cy.get('#password').clear().type(password[0])
+            cy.get('#submit').click()
+
+            cy.get('#error').should('contain.text', 'Your password is invalid!')
+        })        
     });
 
     // it("Logi with fixture", () => {
