b9d578e2fb3794edc2ade79a53bb4925b3eef7a9
prayogoferry <prayogoferry@gmail.com>
2023-07-20 15:57:14 +0700

    none

--- cypress/e2e/0-personal-learn/fixture.cy.js
+++ cypress/e2e/0-personal-learn/fixture.cy.js
@@ -3,6 +3,21 @@
 describe (()=>{
 
     beforeEach(()=>{
-        cy.visit('http://zero.webappsecurity.com/login.html')
+        cy.visit('sauceddemo.com')
+       
     })
+
+    
+
+
+    it("Logi with fixture", () => {
+        cy.fixture('saucedemo').then(demo=>{
+            const username = demo.username
+            const password = demo.password
+
+            cy.get('.input').clear()
+            cy.get('.input').type(username(1))
+        })
+
+    });
 })
--- cypress/e2e/0-personal-learn/login_test.cy.js
+++ cypress/e2e/0-personal-learn/login_test.cy.js
@@ -11,9 +11,29 @@ describe('working with inputs ',()=>{
         cy.get('#user_login').clear().type('username')
         cy.get('#user_password').clear().type('password')
         cy.get('#user_remember_me').check()
-        cy.get('.btn').click(2)
+        cy.get('.btn').click(2)   
+    })
         
-        
-    });
+    it('Should try to login with fixture', ()=> {
+        cy.fixture('user').then(user=> {
+           const username = user.username
+           const password = user.password
+
+           cy.get('#user_login').clear()
+           cy.get('#user_login').type(username)
+
+           cy.get('#user_password').clear()
+           cy.get('#user_password').type(password)
+
+           cy.get('#user_remember_me').check()
+           cy.get('.btn').click()
+
+           cy.get('.alert').should('contain.text', 'Login and/or password are wrong.')//class mengggukan titik(.) untuk menunjuk class di elemen
+        })
+
+    })
+
+
+    
 
 })
