306a5202635fdb3d84b31bc666eda83926fd7a55
prayogoferry <prayogoferry@gmail.com>
2023-08-09 19:37:56 +0700

    api post

--- cypress/e2e/3-integration/api/get-user-list.cy.js
+++ cypress/e2e/3-integration/api/get-user-list.cy.js
@@ -5,7 +5,9 @@ describe('Get Users', ()=>{
         cy.request({
             method : "GET",
             url:"https://reqres.in/api/users?page=2&per_pages=1&delay=3"
+           // url:"https://reqres.in/api/"
         }).as('users')
         cy.get("@users").its('status').should('equal', 200)
+       // cy.get("@users").its('pretty').should('contain',"Lawson")
     });
 })
