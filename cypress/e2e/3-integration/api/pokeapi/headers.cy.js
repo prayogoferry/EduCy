4ff72d650107b450e4276ad28c7c93656b560eb8
prayogoferry <prayogoferry@gmail.com>
2023-08-14 20:48:54 +0700

    header test

--- cypress/e2e/3-integration/api/pokeapi/headers.cy.js
+++ cypress/e2e/3-integration/api/pokeapi/headers.cy.js
@@ -6,20 +6,29 @@ describe('validate headers', ()=>{
 
         //tugas 
 
-        // cy.get('@pokemon').then((response)=>{
-        //     expect(response.status).equal(200)
-        //     expect(response.body.abilities).should('include', 'limber')
-        // })
-
-        cy.request("GET", "https://pokeapi.co/api/v2/pokemon/ditto")
-        .then((response)=>{
-            expect(response.body.abilities).contain('limber')
-            //expect(response.body).contain('abilities')
+        cy.get('@pokemon').then((response)=>{
+            expect(response.status).equal(200)
+           // expect(response.).should('include', 'limber')
         })
 
-        cy.get('@pokemon').its('body').its("abilities")
-        .should('include', "ability")
+        // cy.request("GET", "https://pokeapi.co/api/v2/pokemon/ditto")
+        // .then((response)=>{
+        //     expect(response.body.abilities).contain('limber')
+        //     //expect(response.body).contain('abilities')
+        // })
 
-        
+        cy.get('@pokemon').its('body').its("abilities").its('url')
+        .should('include', "ability")
     });
-})
+
+    it('validate', () => {
+        cy.request("https://pokeapi.co/api/v2/pokemon/charmander").as('poke')
+        cy.get('@poke').its('headers').its('content-encoding').should('include', "gzip")
+        cy.get('@poke').its('body').its('name').should('contain', 'charmander')
+
+       // cy.get('@poke').its('body').its('abilities').its("ability").should('contain', 'ablilities')
+       cy.get('@poke').then((response)=>{
+        expect(response.body.name).contain('charmander')        
+       })
+        })
+    });
