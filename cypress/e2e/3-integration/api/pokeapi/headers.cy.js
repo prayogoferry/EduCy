86ad4a7bfb67c2e94f680a5c1957fb0243898226
prayogoferry <prayogoferry@gmail.com>
2023-08-16 23:20:14 +0700

    task validate headers

--- cypress/e2e/3-integration/api/pokeapi/headers.cy.js
+++ cypress/e2e/3-integration/api/pokeapi/headers.cy.js
@@ -9,28 +9,21 @@ describe('validate headers', ()=>{
         cy.get('@pokemon').then((response)=>{
             expect(response.status).equal(200)
             expect(response.body.name).contain('ditto')
-           // expect(response.body.abilities).contain({url:'https://pokeapi.co/api/v2/ability/7'})
+            expect(response.body.abilities[0].ability.url).contain('https://pokeapi.co/api/v2/ability/7/')
             
         })
 
-        cy.request("GET", "https://pokeapi.co/api/v2/pokemon/ditto")
-        .then((response)=>{
-            expect(response.body.abilities.ability).contain({url : 'https://pokeapi.co/api/v2/ability/7'})
-            //expect(response.body).contain('abilities')
-        })
-
-        cy.get('@pokemon').its('body').its('name')
-        .should('include', "ditto")
+       
     });
 
-    it('validate', () => {
-        cy.request("https://pokeapi.co/api/v2/pokemon/charmander").as('poke')
-        cy.get('@poke').its('headers').its('content-encoding').should('include', "gzip")
-        cy.get('@poke').its('body').its('name').should('contain', 'charmander')
+    // it('validate', () => {
+    //     cy.request("https://pokeapi.co/api/v2/pokemon/charmander").as('poke')
+    //     cy.get('@poke').its('headers').its('content-encoding').should('include', "gzip")
+    //     cy.get('@poke').its('body').its('name').should('contain', 'charmander')
 
-       // cy.get('@poke').its('body').its('abilities').its("ability").should('contain', 'ablilities')
-       cy.get('@poke').then((response)=>{
-        expect(response.body.name).contain('charmander')        
-       })
-        })
+    //    // cy.get('@poke').its('body').its('abilities').its("ability").should('contain', 'ablilities')
+    //    cy.get('@poke').then((response)=>{
+    //     expect(response.body.name).contain('charmander')        
+    //    })
+    //     })
     });
